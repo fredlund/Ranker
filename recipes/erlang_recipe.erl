e61338f46684d1e79695154a16ef462fbb8ddce2
Lars-Ake Fredlund <lfredlund@fi.upm.es>
2015-10-22 12:21:47 +0200

    Fix

--- recipes/erlang_recipe.erl
+++ recipes/erlang_recipe.erl
@@ -1,23 +1,11 @@
 -module(erlang_recipe).
 
--include("implementation.hrl").
-
--define(ETS_TABLE,ranker_erlang_ets).
-
 -export([start/2,start_implementation/1,stop_implementation/2,finish/0]).
 
-start(Implementations,_) ->
-  ranker_create_ets_table:ensure_open(?ETS_TABLE),
-  ImplementationIds =
-    lists:map
-      (fun (Implementation) ->
-	   {Implementation#implementation.id,Implementation}
-       end, Implementations), 
-  ets:insert(?ETS_TABLE,{implementations,ImplementationIds}).
-  
-start_implementation(Id) ->
-  [{_,ImplementationIds}] = ets:lookup(?ETS_TABLE,implementations),
-  {_,_Implementation} = lists:keyfind(Id,1,ImplementationIds),
+start(_Implementations,_) ->
+  ok.
+
+start_implementation(_) ->
   ok.
 
 stop_implementation(_,_) ->
